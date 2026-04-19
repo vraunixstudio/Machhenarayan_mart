300f321e265237359358be59b3c7efc7934b453e
Developer <developer@example.com>
2026-04-19 08:08:29 +0545

    Try async connection

--- backend/server.js
+++ backend/server.js
@@ -100,19 +100,20 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
+let isConnected = false;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-console.log('Connecting to MongoDB...');
-if (MONGODB_URI) {
-  mongoose.connect(MONGODB_URI, { serverSelectionTimeoutMS: 5000 })
-    .then(() => {
-      console.log('Connected to MongoDB!');
-    })
-    .catch(err => {
+async function connectDB() {
+  if (MONGODB_URI && !isConnected) {
+    try {
+      await mongoose.connect(MONGODB_URI);
+      isConnected = true;
+      console.log('MongoDB connected!');
+    } catch (err) {
       console.error('MongoDB error:', err.message);
-    });
-} else {
-  console.error('MONGODB_URI not set!');
+    }
+  }
 }
+connectDB();
 
 module.exports = app;
