5e28b68ab1a6b9e105881246ec7ab9550a1d9bd4
Developer <developer@example.com>
2026-04-19 12:50:33 +0545

    Keep lazy loading and fix config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,15 +15,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
-  },
-  appType: 'spa'
+    chunkSizeWarningLimit: 1000
+  }
 });
